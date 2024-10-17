c40e3ebbb32c937c87538345cf2f8817e6005df4
Water1ock <varmashubhanjay@gmail.com>
2024-10-17 21:19:36 +0100

    Implement Transformer Model

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from model_components.encoder import Encoder
+from model_components.decoder import Decoder
+from model_components.input_embedding import InputEmbedding
+from model_components.positional_encoding import PositionalEncoding
+from model_components.projection_layer import ProjectionLayer
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbedding, tgt_embed: InputEmbedding, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+
+    def decode(self, encoder_output: torch.Tensor, src_mask: torch.Tensor, tgt: torch.Tensor, tgt_mask: torch.Tensor):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+
+    def project(self, x):
+        return self.projection_layer(x)
