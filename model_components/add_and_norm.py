ab895c9f989e4723711af10fa029f9fb68ab876b
Water1ock <varmashubhanjay@gmail.com>
2024-10-17 21:41:43 +0100

    Added features to Add And Norm Block

--- model_components/add_and_norm.py
+++ model_components/add_and_norm.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 
 class AddAndNorm(nn.Module):
-    def __init__(self, eps: float = 10**-6):
+    def __init__(self, features: int, eps: float = 10**-6):
         super().__init__()
         self.eps = eps
-        self.alpha = nn.Parameter(torch.ones(1))
-        self.bias = nn.Parameter(torch.zeros(1))
+        self.alpha = nn.Parameter(torch.ones(features))
+        self.bias = nn.Parameter(torch.zeros(features))
 
     def forward(self, x):
         mean = x.mean(dim = -1, keepdim = True)
