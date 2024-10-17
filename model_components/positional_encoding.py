9bdbb73f38636c65fea5fefdbddf31620388db08
Water1ock <varmashubhanjay@gmail.com>
2024-10-17 19:56:20 +0100

    bugFix in positional_encoding.py

--- model_components/positional_encoding.py
+++ model_components/positional_encoding.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 from constants import POSITIONAL_SCALE
 
 
@@ -12,15 +13,14 @@ class PositionalEncoding(nn.Module):
 
         positional_encoding_matrix = torch.zeros(sequence_length, d_model)
         position = torch.arange(0, sequence_length, dtype=torch.float).unsqueeze(1)
-        i_indices = torch.arange(0, d_model).float()
-        div_term = torch.pow(POSITIONAL_SCALE, 2*i_indices/d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(POSITIONAL_SCALE) / d_model))
 
-        positional_encoding_matrix[:, 0::2] = torch.sin(position / div_term[0::2])
-        positional_encoding_matrix[:, 1::2] = torch.cos(position / div_term[1::2])
+        positional_encoding_matrix[:, 0::2] = torch.sin(position * div_term[0::2])
+        positional_encoding_matrix[:, 1::2] = torch.cos(position * div_term[1::2])
 
         positional_encoding_matrix = positional_encoding_matrix.unsqueeze(0)
         self.register_buffer('positional_encoding_matrix', positional_encoding_matrix)
 
     def forward(self, embeddings):
-        embeddings = embeddings + self.positional_encoding_matrix[:, :embeddings.shape[1], :]
+        embeddings = embeddings + (self.positional_encoding_matrix[:, :embeddings.shape[1], :]).requires_grad_(False)
         return self.dropout(embeddings)
