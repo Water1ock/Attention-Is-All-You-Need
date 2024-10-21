c2a53a024148757f7da1635b0c9806a033e2ba1a
Water1ock <varmashubhanjay@gmail.com>
2024-10-21 23:28:05 +0100

    bugFix in input_embedding.py

--- model_components/input_embedding.py
+++ model_components/input_embedding.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class InputEmbedding(nn.Module):
-    def __init__(self, vocab_size: int, d_model: int):
+    def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
         self.vocab_size = vocab_size
         self.d_model = d_model
