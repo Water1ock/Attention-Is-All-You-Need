0aac9c5d3304352dd387349160ed4715b12704d4
Water1ock <varmashubhanjay@gmail.com>
2024-10-24 16:55:30 +0100

    add translate.py for machine translation tasks

--- config.py
+++ config.py
@@ -22,3 +22,13 @@ def get_weights_file_path(config, epoch: str):
     model_basename = config['model_basename']
     model_filename = f"{config['model_basename']}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
+
+def latest_weights_file_path(config):
+    model_folder = f"{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    print(weights_files)
+    return str(weights_files[-1])
